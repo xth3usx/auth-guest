88aa2c70f2e30573fdbf54b3c49b7493f71ead32
xth3usx <matheus.hso89@gmail.com>
2024-10-25 12:37:27 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 // Configurações OAuth2 para a API Gov.br
-define('CLIENT_ID', 'seu_client_id_aqui');
-define('CLIENT_SECRET', 'seu_client_secret_aqui');
-define('REDIRECT_URI', 'https://seudominio/callback.php');
+define('CLIENT_ID', 'ID_CLIENTE_GOV_BR');
+define('CLIENT_SECRET', 'SEGREDO_CLIENTE_GOV_BR');
+define('REDIRECT_URI', 'HTTPS://RETORNO_REGISTRADO_GOV_BR/CALLBACK.PHP');
 define('AUTH_URL', 'https://sso.staging.acesso.gov.br/authorize');
 define('TOKEN_URL', 'https://sso.staging.acesso.gov.br/token');
 define('USERINFO_URL', 'https://sso.staging.acesso.gov.br/userinfo');
